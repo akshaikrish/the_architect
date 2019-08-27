e89cbd920b9c7eb9c3d2a1f55fa46619b0877344
akshaikrish <akshaikrishna@gmail.com>
2019-08-27 16:50:59 +0530

    Modal video pausing issue addressed. Fine tuning done for carousel

--- src/pages/architectPage/architectPage.js
+++ src/pages/architectPage/architectPage.js
@@ -1,4 +1,7 @@
 import "bootstrap/js/src/index";
 import $ from "jquery";
+import videolink from "../../components/videolink/videolink.js";
+
+videolink();
 
 // import "../../components/carousel/carousel.js";
